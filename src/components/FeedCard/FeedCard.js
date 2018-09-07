676c574cff7df3476882d47b5ee2985fafd36a1a
Faza Fahleraz <ffahleraz@gmail.com>
2018-09-07 23:06:02 +0700

    fix missing image

--- src/components/FeedCard/FeedCard.js
+++ src/components/FeedCard/FeedCard.js
@@ -6,28 +6,95 @@ import styles from './Styles'
 class FeedCard extends React.Component {
   render() {
     return(
-      <TouchableOpacity onPress={ this.props.onPress }>
+      <TouchableOpacity onPress={this.props.onPress}>
         <View
-          style={{ flexDirection: 'row', backgroundColor: 'white', borderRadius: 12, marginLeft: 50, marginRight: 16, marginTop: 7, marginBottom: 7, height: 154, flex: 1, alignItems: 'center', shadowOpacity: 0.08, shadowOffset: { width: 0, height: 4 }, shadowRadius: 16 }}
+          style={{ 
+            flexDirection: 'row', 
+            backgroundColor: 'white', 
+            borderRadius: 12, 
+            marginLeft: 50, 
+            marginRight: 16, 
+            marginTop: 7, 
+            marginBottom: 7, 
+            height: 154, 
+            flex: 1, 
+            alignItems: 'center', 
+            shadowOpacity: 0.08, 
+            shadowOffset: { width: 0, height: 4 }, 
+            shadowRadius: 16
+          }}
         >
           <View
-            style={{ shadowOpacity: 0.16, shadowOffset: { width: 0, height: 4 }, shadowRadius: 16 }}
+            style={{
+              shadowOpacity: 0.16, 
+              shadowOffset: { width: 0, height: 4 }, 
+              shadowRadius: 16,
+              backgroundColor: 'black',
+              left: -34,
+              borderRadius: 12
+            }}
           >
             <Image
-              style={{width: 126, height: 126, borderRadius: 12, position: 'relative', left: -34, }}
-              source={{ uri: this.props.uri }}
+              style={{
+                width: 126, 
+                height: 126,
+                borderRadius: 12,
+                backgroundColor: 'gray'
+              }}
+              source={{uri: this.props.imageUri}}
             />
           </View>
           <View
-            style={{ flexDirection: 'column', flex: 1, marginLeft: 0, justifyContent: 'center', position: 'relative', left: -18, height: 154 }}
+            style={{
+              flexDirection: 'column', 
+              flex: 1, 
+              marginLeft: 0, 
+              justifyContent: 'center', 
+              position: 'relative', 
+              left: -18, 
+              height: 154
+            }}
           >
-            <Text style={{ fontFamily: 'System', fontWeight: '700',  textAlign: 'left', fontSize: 20, fontWeight: 'bold', color: '#454F53', marginBottom: 2 }}>
+            <Text 
+              style={{
+                fontFamily: 'System', 
+                fontWeight: '700',
+                textAlign: 'left', 
+                fontSize: 20, 
+                fontWeight: 'bold', 
+                color: '#454F53', 
+                marginBottom: 2 
+              }}
+            >
               {this.props.title}
             </Text>
-            <Text style={{ fontFamily: 'System', fontWeight: '600', fontStyle: 'italic' ,  textAlign: 'left', fontSize: 12, fontStyle: 'italic', color: '#78849E', marginBottom: 2 }}>
+            <Text 
+              style={{ 
+                fontFamily: 'System', 
+                fontWeight: '600', 
+                fontStyle: 'italic', 
+                textAlign: 'left', 
+                fontSize: 12, 
+                fontStyle: 'italic', 
+                color: '#78849E', 
+                marginBottom: 2 
+              }}
+            >
               {this.props.author}
             </Text>
-            <Text style={{ fontFamily: 'System', fontWeight: '500', marginLeft: 10, textAlign: 'right', fontSize: 16, color: '#696969', position: 'absolute', bottom: 14, right: 0  }}>
+            <Text 
+              style={{
+                fontFamily: 'System', 
+                fontWeight: '500', 
+                marginLeft: 10, 
+                textAlign: 'right', 
+                fontSize: 16, 
+                color: '#696969', 
+                position: 'absolute', 
+                bottom: 14, 
+                right: 0
+              }}
+            >
               {this.props.date}
             </Text>
           </View>
@@ -38,14 +105,14 @@ class FeedCard extends React.Component {
 }
 
 function renderFeedCard(item, onPress) {
-  return(
+  return (
     <FeedCard
       uri={item.item.uri}
       imageUri={item.item.imageUri}
       title={item.item.title}
       author={item.item.author}
       date={item.item.date}
-      onPress={() => {onPress(item.item.uri)}}
+      onPress={() => { onPress(item.item.uri); }}
     />
   );
 }
